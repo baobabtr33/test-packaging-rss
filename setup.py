0def2381e184f378b1309209fe5a7b1917ee592d
JungHwan Kim <59995362+baobabtr33@users.noreply.github.com>
2020-12-08 15:26:42 +0900

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='test-packaging-rss',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/baobabtr33/test-packaging-rss',
+    license='',
+    author='Kim Jung Hwan',
+    author_email='',
+    description='test packaging'
+)
+
