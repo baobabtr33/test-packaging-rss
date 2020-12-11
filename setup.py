8c29fe554ec1d5ae1c7fc11b7f7a1527266b3d16
Kim Jung Hwan <stevekim01310@gmial.com>
2020-12-11 09:37:18 +0900

    packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='test-packaging-rss',
-    version='0.0.5',
+    version='0.0.7',
     url='https://github.com/baobabtr33/test-packaging-rss',
     license='',
     author='Kim Jung Hwan',
